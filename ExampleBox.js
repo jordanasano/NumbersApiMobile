c233cfeee5ed7679788a52afb7cd244de6703a58
Sara <saramaheath@gmail.com>
2022-08-17 12:01:34 -0500

    fixed bug for syncing facts and paths in all display locations

--- ExampleBox.js
+++ ExampleBox.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { Text, View, ActivityIndicator } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {Text, View, ActivityIndicator} from 'react-native';
 
 /** ExampleBox
  *
@@ -12,49 +12,55 @@ import { Text, View, ActivityIndicator } from 'react-native';
  * ExampleContainer -> ExampleBox
  */
 
-const ExampleBox = ({ apiCall, category, updateWidget }) => {
-    const [fact, setFact] = useState(null);
+const ExampleBox = ({apiCall, category, updateWidget}) => {
+  const [factData, setFactData] = useState({fact: null, path: null});
 
-    const randomNum = Math.floor(Math.random() * 255);
-    let baseURL = `http://numbersapi.com/`;
-    let path = null;
-    if (category === 'Math') {
-        path = `${randomNum}/math`;
-    }
-    if (category === 'Trivia') {
-        path = `${randomNum}`;
-    }
-    if (category === 'Date') {
-        const today = new Date();
-        const dd = String(today.getDate()).padStart(2, '0');
-        const mm = String(today.getMonth() + 1).padStart(2, '0');
-        path = `${mm}/${dd}/date`
-    }
+  const randomNum = Math.floor(Math.random() * 255);
+  let baseURL = `http://numbersapi.com/`;
+  let path = null;
+  if (category === 'Math') {
+    path = `${randomNum}/math`;
+  }
+  if (category === 'Trivia') {
+    path = `${randomNum}`;
+  }
+  if (category === 'Date') {
+    const today = new Date();
+    const dd = String(today.getDate()).padStart(2, '0');
+    const mm = String(today.getMonth() + 1).padStart(2, '0');
+    path = `${mm}/${dd}/date`;
+  }
 
-    useEffect(() => {
-        async function populateFact() {
-            const response = await apiCall(baseURL + path);
-            const fact = response.data;
-            setFact(fact);
-        }
-        populateFact();
-    }, []);
-
-    function handlePress() {
-        updateWidget(fact, path);
+  useEffect(() => {
+    async function populateFact() {
+      const response = await apiCall(baseURL + path);
+      const fact = response.data;
+      setFactData({fact: fact, path: path});
     }
+    populateFact();
+  }, []);
 
-    if (fact === null) {
-        return <ActivityIndicator size="small" />;
+  function handlePress() {
+    async function populateFact() {
+      const response = await apiCall(baseURL + path);
+      const fact = response.data;
+      setFactData({fact: fact, path: path});
+      updateWidget(fact, path);
     }
+    populateFact();
+  }
 
-    return (
-        <View>
-            <Text>{category}</Text>
-            <Text onPress={handlePress}>{baseURL + path}</Text>
-            <Text>{fact}</Text>
-        </View>
-    )
+  if (factData.fact === null) {
+    return <ActivityIndicator size="small" />;
+  }
+
+  return (
+    <View>
+      <Text>{category}</Text>
+      <Text onPress={handlePress}>{baseURL + factData.path}</Text>
+      <Text>{factData.fact}</Text>
+    </View>
+  );
 };
 
 export default ExampleBox;
