81a5d98b8fd03cf247ee919250b7516edc0af9fc
Sara <saramaheath@gmail.com>
2022-08-17 12:54:26 -0500

    worked on submitting fact form, won't continue to pursue that, add to todo list for further study

--- FactForm.js
+++ FactForm.js
@@ -1,42 +1,52 @@
-import axios from "axios";
-import React, {useState} from "react";
-import { TextInput, Text, Alert } from "react-native";
-import styles from "./App";
+import axios from 'axios';
+import React, {useState} from 'react';
+import {TextInput, Text, Alert} from 'react-native';
+import styles from './App';
+import qs from 'qs';
 
 /**Fact Form
- * 
- * -state: 
+ *
+ * -state:
  * -factData (obj)
- * 
- * -props: 
+ *
+ * -props:
  * -submitFact
- * 
+ *
  * WidgetContainer -> FactForm
  */
 
-const FactForm = ({ submitFact, handleSubmitClick }) => {
-    const [fact, setFact] = useState("");
-    async function submitFact() {
-        try {
-            const url = 'http://numbersapi.com/submit'
-            // Our post request isn't going through. data might not be formatted right
-            await axios.post(url, {data:{"fact":fact}});
-        } catch {
-            Alert.alert(`Don't know how to open this URL:`);
-        }
-        handleSubmitClick();
+const FactForm = ({submitFact, handleSubmitClick}) => {
+  const [fact, setFact] = useState('');
+  async function submitFact() {
+    try {
+      const url = 'http://numbersapi.com/submit';
+      // Our post request isn't going through. data might not be formatted right
+      const data = qs.stringify({
+          text: 'new fact',
+          number: '42',
+          type: 'trivia',
+        });
+
+      const options = {
+        method: 'POST',
+        headers: { 'content-type': 'application/x-www-form-urlencoded' },
+        data: qs.stringify(data),
+        url,
+      };
+      await axios(options);
+      
+    } catch {
+      Alert.alert(`Don't know how to open this URL:`);
     }
-    return (
-        <>
-            <Text>Type in a number and a fact!</Text>
-            <TextInput
-                style={styles.input}
-                onChangeText={setFact}
-                value={fact}
-            />
-            <Text onPress={submitFact}>Submit your fact!</Text>
-        </>
-    )
-} 
+    handleSubmitClick();
+  }
+  return (
+    <>
+      <Text>Type in a number and a fact!</Text>
+      <TextInput style={styles.input} onChangeText={setFact} value={fact} />
+      <Text onPress={submitFact}>Submit your fact!</Text>
+    </>
+  );
+};
 
 export default FactForm;
